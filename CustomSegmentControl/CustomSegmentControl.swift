9e05b8f7dbbfda18f213b11a0d9f32b764db99f3
Mohamed-Khaterr <mohamed.15751@gmail.com>
2022-11-21 16:19:42 +0200

    initial commit

---
+++ CustomSegmentControl/CustomSegmentControl.swift
@@ -0,0 +1,159 @@
+//
+//  CustomSegmentControl.swift
+//  CustomSegmentControl
+//
+//  Created by Khater on 11/21/22.
+//
+
+import UIKit
+
+// MARK: - Protocol
+protocol CustomSegmentControlDelegate: AnyObject{
+    func didIndexChange(at index: Int)
+}
+
+// MARK: - Class
+@IBDesignable class CustomSegmentControl: UIView{
+    
+    // MARK: Properties
+    public weak var delegate: CustomSegmentControlDelegate?
+    
+    private var buttonsStackView: UIStackView?
+    private var lineView: UIView?
+    private var selectedButtonLineView: UIView?
+    
+    private var buttons: [UIButton] = []
+    
+    // MARK: - IBInspectable's
+    @IBInspectable var textColor: UIColor = .blue{
+        didSet{
+            for button in buttons {
+                button.tintColor = textColor
+            }
+        }
+    }
+    
+    @IBInspectable var lineColor: UIColor = .black {
+        didSet{
+            selectedButtonLineView?.backgroundColor = lineColor
+        }
+    }
+    
+    @IBInspectable var commaSeparatedButtonTitles: String = ""{
+        didSet{
+            buttons.removeAll()
+            
+            let buttonTitles = commaSeparatedButtonTitles.components(separatedBy: ",")
+            
+            for buttonTitle in buttonTitles {
+                let button = UIButton(type: .system)
+                button.setTitle(buttonTitle, for: .normal)
+                button.titleLabel?.font = UIFont(name: "Inter-Regular", size: 17)
+                button.titleLabel?.numberOfLines = 0
+                button.addTarget(self, action: #selector(selectButton(_:)), for: .touchUpInside)
+                
+                buttons.append(button)
+            }
+            
+            updateView()
+        }
+    }
+    
+    
+    // MARK: - Update UI
+    private func updateView(){
+        removeAll()
+        setupUI()
+        addToSubview()
+        setupConstraints()
+    }
+    
+    private func removeAll(){
+        subviews.forEach { view in
+            view.removeFromSuperview()
+        }
+        buttonsStackView = nil
+        lineView = nil
+        selectedButtonLineView = nil
+    }
+    
+    private func setupUI(){
+        setupButtonsStackView()
+        setupSelectedButtonLineView()
+        setupLineView()
+    }
+    
+    private func addToSubview(){
+        addSubview(buttonsStackView!)
+        lineView?.addSubview(selectedButtonLineView!)
+        addSubview(lineView!)
+    }
+    
+    private func setupConstraints(){
+        addConstraintToButtonsStackView()
+        addConstraintToLineView()
+        addConstraintToSelectedButtonLineView()
+    }
+    
+    // MARK: - Setup UI
+    private func setupButtonsStackView(){
+        buttonsStackView = UIStackView(arrangedSubviews: buttons)
+        buttonsStackView?.axis = .horizontal
+        buttonsStackView?.alignment = .fill
+        buttonsStackView?.distribution = .fillEqually
+    }
+    
+    private func setupLineView(){
+        lineView = UIView()
+        lineView?.backgroundColor = .lightGray
+    }
+    
+    private func setupSelectedButtonLineView(){
+        selectedButtonLineView = UIView()
+        selectedButtonLineView?.backgroundColor = .black
+    }
+    
+    // MARK: - Constraint's
+    private func addConstraintToButtonsStackView(){
+        buttonsStackView?.translatesAutoresizingMaskIntoConstraints = false
+        buttonsStackView?.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        buttonsStackView?.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        buttonsStackView?.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+    }
+    
+    private func addConstraintToLineView(){
+        lineView?.translatesAutoresizingMaskIntoConstraints = false
+        lineView?.heightAnchor.constraint(equalToConstant: 2).isActive = true
+        lineView?.topAnchor.constraint(equalTo: buttonsStackView!.bottomAnchor).isActive = true
+        lineView?.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        lineView?.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        lineView?.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+    }
+    
+    private func addConstraintToSelectedButtonLineView(){
+        selectedButtonLineView?.translatesAutoresizingMaskIntoConstraints = false
+        selectedButtonLineView?.heightAnchor.constraint(equalToConstant: 2).isActive = true
+        selectedButtonLineView?.widthAnchor.constraint(equalToConstant: (frame.width / CGFloat(buttons.count))).isActive = true
+        selectedButtonLineView?.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        selectedButtonLineView?.topAnchor.constraint(equalTo: buttonsStackView!.bottomAnchor).isActive = true
+        selectedButtonLineView?.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+    
+    // MARK: - Button Action
+    @objc private func selectButton(_ button: UIButton){
+        let selectedButtonIndex = buttons.firstIndex(of: button)!
+        
+        if delegate != nil {
+            // The delegate will change the selectedButtonLineView position
+            delegate!.didIndexChange(at: selectedButtonIndex)
+            
+        } else {
+            guard selectedButtonLineView?.transform.tx != button.frame.origin.x else { return }
+            
+            UIView.animate(withDuration: 0.3) {
+                self.selectedButtonLineView?.transform.tx = button.frame.origin.x
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Custom Segment Control
+Create segment control like instagram pofile followers and following.
+
+<video autoplay src="demo/customSegmentControlDemo.mp4" width="40%" >demo video</video>
+
+## Attribute Inspecter
+- Text Color: is the button tintColor.
+- Line Color: is the background color of the view under selected button to indicate which button is selected.
+- Comma Separated Button Titles: convert the input text into array of Strings(titles) that separated by comma (`,`) then create button and set title to element in the array
+
+## Delegate
+```swift
+protocol CustomSegmentControlDelegate: AnyObject{
+    func didIndexChange(at index: Int) // When the button is selected
+}
+```
